c4174c8c1166c813126a200df776ec0e557855d9
KamalMusayev <kmusayev038@gmail.com>
2025-06-11 15:53:29 +0400

    Update 12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1,101):
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
         elif i % 3 == 0:
@@ -9,4 +8,5 @@ def fizzbuzz():
             print("Buzz", end=" ")
         else:
             print(i, end=" ")
-print(fizzbuzz())
+fizzbuzz() 
+print()   
