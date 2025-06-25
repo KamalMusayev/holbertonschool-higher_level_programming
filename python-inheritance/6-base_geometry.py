128a31ead6725e14d36107a7707e0984c4b89a2c
KamalMusayev <kmusayev038@gmail.com>
2025-06-25 13:08:04 +0000

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class begins from here."""
+
+
+class BaseGeometry:
+    """
+    This is a Class.
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
