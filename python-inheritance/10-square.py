8cbe533aa9fddd1e9549858f59659136c7d95180
KamalMusayev <kmusayev038@gmail.com>
2025-06-26 17:02:29 +0400

    Update 10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,42 +1,19 @@
 #!/usr/bin/python3
-"""Class begins from here."""
+"""Square class that inherits from Rectangle"""
 
+Rectangle = __import__('8-rectangle').Rectangle
 
-class BaseGeometry:
-    """
-    This is a Class.
-    """
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-class Rectangle(BaseGeometry):
-    """Rectangle inherits from BaseGeometry"""
-
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        print(f"[Rectangle] {self.__width}/{self.__height}")
 
 class Square(Rectangle):
-    """Square inherits from BaseGeometry"""
+    """Square inherits from Rectangle"""
 
     def __init__(self, size):
         self.integer_validator("size", size)
+        super().__init__(size, size)  # Call Rectangle initializer
         self.__size = size
 
     def area(self):
         return self.__size ** 2
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
