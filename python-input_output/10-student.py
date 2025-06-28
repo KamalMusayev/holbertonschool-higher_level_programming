31debaecdc8b61331a5c273a79a70c8642862237
KamalMusayev <kmusayev038@gmail.com>
2025-06-28 16:32:06 +0000

    10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Class start from here!."""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Inside of Class
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            filtered = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    filtered[attr] = getattr(self, attr)
+            return filtered
+        else:
+            return self.__dict__.copy()
