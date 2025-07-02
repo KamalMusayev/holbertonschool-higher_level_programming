c5bff8b1356e1dd89cc7cfe951f341249d22d0a6
KamalMusayev <kmusayev038@gmail.com>
2025-07-02 19:03:27 +0400

    Update 8-cities_of_california_subquery.sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- COMMENT
-SELECT name, id FROM states WHERE states.id =(
-	SELECT id
+SELECT id, name FROM cities WHERE states.id =(
+    SELECT id
     FROM states
     WHERE name = 'California'
 )
